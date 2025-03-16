4f872a0eb2ce0256fc3a2ae44f67100aa73a8061
Donald Hawkins <donald@hawkins.pro>
2025-03-16 18:18:15 -0400

    refactored serializer funcs into StixProcessor class

--- src/attck_stix_agent/_serialize/__init__.py
+++ src/attck_stix_agent/_serialize/__init__.py
@@ -1,23 +1,119 @@
-import io
-import json
-from typing import Any
+from collections.abc import Mapping, Sequence
+from typing import ClassVar
 
-from stix2.v20.sdo import IntrusionSet
-
-# TODO: add `external_references` once they can be unmarshalled
-GROUP_KEEP_KEYS = (
-    "id",
-    "name",
-    "description",
-    "aliases",
-    "object_marking_refs",
-)
+from stix2.v20.sdo import AttackPattern, ExternalReference, IntrusionSet
 
 
-def group_to_dict(group: IntrusionSet) -> dict[str, Any]:
-    group_dict: dict = {}
-    for k, v in group.items():
-        if k not in GROUP_KEEP_KEYS:
-            continue
-        group_dict[k] = v
-    return group_dict
+class StixProcessor:
+    DEFAULT_GROUP_KEEP_KEYS: ClassVar[tuple[str, ...]] = (
+        "id",
+        "name",
+        "description",
+        "aliases",
+        "external_references",
+    )
+    DEFAULT_TECHNIQUE_KEEP_KEYS: ClassVar[tuple[str, ...]] = (
+        "id",
+        "name",
+        "description",
+        "kill_chain_phases",
+        "external_references",
+        "x_mitre_data_sources",
+    )
+
+    def __init__(self) -> None:
+        self.group_keep_keys: tuple[str, ...] = self.DEFAULT_GROUP_KEEP_KEYS
+        self.technique_keep_keys: tuple[str, ...] = self.DEFAULT_TECHNIQUE_KEEP_KEYS
+
+    @classmethod
+    def stix_to_dict(
+        cls, stix_obj: Mapping, keep_keys: Sequence[str] | None = None
+    ) -> dict:
+        stix_dict: dict = {}
+        for k, stix_val in stix_obj.items():
+            if keep_keys and k not in keep_keys:
+                continue
+            if stix_val is None or isinstance(stix_val, str):
+                stix_dict[k] = stix_val
+            elif isinstance(stix_val, ExternalReference):
+                stix_dict[k] = cls._external_ref_to_dict(stix_val)
+            elif isinstance(stix_val, Sequence):
+                new_stix_val = []
+                for i in stix_val:
+                    if isinstance(i, ExternalReference):
+                        new_stix_val.append(cls._external_ref_to_dict(i))
+                    else:
+                        new_stix_val.append(i)
+                stix_dict[k] = new_stix_val
+            else:
+                stix_dict[k] = stix_val
+        return stix_dict
+
+    @staticmethod
+    def kill_chain_phases(
+        technique: AttackPattern, kill_chain: str | None = None
+    ) -> dict[str, list[str]] | list[str]:
+        """List all phases of all kill chains that the technique applies to.
+
+        Args:
+            technique (AttackPattern):
+                A STIX technique entry.
+            kill_chain (str, optional):
+                Return only phases for the given kill_chain.
+                All kill_chains are returned if None. Defaults to None.
+
+        Raises:
+            TypeError: `technique` is not an `AttackPattern`
+
+        Returns:
+            dict[str, list[str]] | list[str]:
+                Mapping of kill chain names and the phases the technique applies to.
+                A list of phases corresponding to `kill_chain` is returned if a
+                `kill_chain` is given.
+        """
+        if not isinstance(technique, AttackPattern):
+            raise TypeError
+
+        kill_chain_phases: dict[str, list[str]] = {}
+        for phase_obj in technique.get("kill_chain_phases", []):
+            kill_chain_name: str = phase_obj.get("kill_chain_name", "unknown")
+            phase_name: str = phase_obj.get("phase_name", "")
+            phases: list[str] = kill_chain_phases.setdefault(kill_chain_name, [])
+            if phase_name not in phases:
+                phases.append(phase_name)
+        if kill_chain:
+            if kill_chain in kill_chain_phases.keys():
+                return kill_chain_phases[kill_chain]
+            return []
+        return kill_chain_phases
+
+    @staticmethod
+    def _external_ref_to_dict(external_ref: ExternalReference) -> dict:
+        # return {k: v for k, v in external_ref.items()}
+        return dict(external_ref)
+
+    def group_to_dict(
+        self, group: IntrusionSet, keep_keys: Sequence[str] | None = None
+    ) -> dict:
+        if keep_keys is None:
+            keep_keys = self.technique_keep_keys
+        return self.stix_to_dict(stix_obj=group, keep_keys=keep_keys)
+
+    def technique_to_dict(
+        self,
+        technique: AttackPattern,
+        keep_keys: Sequence[str] | None = None,
+        kill_chain: str | None = None,
+    ) -> dict:
+        if keep_keys is None:
+            keep_keys = self.technique_keep_keys
+
+        technique_dict: dict = self.stix_to_dict(
+            stix_obj=technique, keep_keys=keep_keys
+        )
+        if keep_keys is None or "kill_chain_phases" in keep_keys:
+            kill_chain_phases: dict[str, list[str]] | list[str] = (
+                self.kill_chain_phases(technique, kill_chain=kill_chain)
+            )
+            technique_dict["kill_chain_phases"] = kill_chain_phases
+        return technique_dict
